db3f5960d482fedf3a6b16c01d0054fb2cf3c04a
“First <Kerger06@aol.com>
2022-01-18 14:39:13 -0600

    aborting mission

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 
 function App() {
+
+  const [currentSum, setCurrentSum] = useState(0);
+  const [clear, setClear] = useState(false)
+
   return (
     <div className="App">
-      Hello World
+      <div className="app-title">
+        <h1> Basic Form Calculator</h1>
+      </div>
+      <form> 
+        <input type="text" id="result" readOnly/>
+        <input type="text" id="num" placeholder="Enter number"/>
+        <button>Add</button>
+        <button>Clear</button>
+      </form>
     </div>
   );
 }
